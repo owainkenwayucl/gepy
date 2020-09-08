b5a920a858902610a8239fa6794caf795e1c73ae
Owain Kenway <o.kenway@ucl.ac.uk>
2020-09-08 18:12:56 +0100

    Starting job class.

---
+++ gepy/__init__.py
@@ -0,0 +1,30 @@
+class job:
+
+    def __init__(self, name='gepy_job', memory=1024, length=60, location='.'):
+        self.name = name
+        self.memory = memory
+        self.length = length
+        self.location = location
+        self.modules = []
+
+    def get_job_script(self):
+        import math
+        script = '#!/bin/bash -l\n'
+        script = script + '#$ -N ' + self.name + '\n'
+        script = script + '#$ -l mem=' + str(self.memory) + 'M\n'
+        hrs = math.floor(self.length/60)
+        mins = self.length%60
+        script = script + '#$ -l h_rt=' + str(hrs) + ':' + str(mins) + ':00 \n'
+
+        if (self.location == '.'):
+            script = script + '#$ -cwd\n'
+        else:
+            script = script + '#$ -wd ' + self.location + '\n'
+
+        for a in self.modules:
+            script = script + 'module load ' + a + '\n'
+
+        return script
+
+        
+
