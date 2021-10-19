6f240ed92a511632db89af296178f810a3930a14
Owain Kenway <o.kenway@ucl.ac.uk>
2021-10-19 16:45:22 +0100

    Try to do a qstat

--- python/gepy/executor.py
+++ python/gepy/executor.py
@@ -2,3 +2,33 @@ def run(command):
     import subprocess
     return subprocess.run(command, capture_output=True, encoding='UTF-8')
     
+def qstat_joblist(filter='*'):
+    import xml.etree.ElementTree as xml
+
+    job_text = executor.run(['/opt/sge/bin/lx-amd64/qstat', '-xml','-u', '*']).stdout
+
+    tree = xml.fromstring(job_text)
+
+    jobs = []
+
+    for child in tree:
+        for subchild in child:
+            status = subchild.attrib['state']
+            jid = subchild.find('JB_job_number').text
+            prio = subchild.find('JAT_prio').text
+            name = subchild.find('JB_name').text
+            owner = subchild.find('JB_owner').text
+            stateblock = subchild.find('state').text
+            if (status == 'pending'):
+                timeblock = subchild.find('JB_submission_time').text
+            else:
+                timeblock = subchild.find('JAT_start_time').text
+            slots = subchild.find('slots').text
+            try:
+                taskinfo = subchild.find('tasks').text
+            except AttributeError as err:
+                taskinfo = None
+            temp_job = queue_job(status, jid, prio, name, owner, stateblock, timeblock, slots, taskinfo)
+            jobs.append(temp_job)
+
+    return jobs
