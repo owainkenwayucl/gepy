b3de89064f03041c33a107d7ab40f54244b43690
Owain Kenway <o.kenway@ucl.ac.uk>
2021-10-19 16:47:25 +0100

    namespaces?

--- python/gepy/executor.py
+++ python/gepy/executor.py
@@ -5,7 +5,7 @@ def run(command):
 def qstat_joblist(filter='*'):
     import xml.etree.ElementTree as xml
 
-    job_text = executor.run(['/opt/sge/bin/lx-amd64/qstat', '-xml','-u', '*']).stdout
+    job_text = gepy.executor.run(['/opt/sge/bin/lx-amd64/qstat', '-xml','-u', '*']).stdout
 
     tree = xml.fromstring(job_text)
 
@@ -28,7 +28,7 @@ def qstat_joblist(filter='*'):
                 taskinfo = subchild.find('tasks').text
             except AttributeError as err:
                 taskinfo = None
-            temp_job = queue_job(status, jid, prio, name, owner, stateblock, timeblock, slots, taskinfo)
+            temp_job = gepy.queue_job(status, jid, prio, name, owner, stateblock, timeblock, slots, taskinfo)
             jobs.append(temp_job)
 
     return jobs
