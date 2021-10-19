596f03de48faf16abde2212f90d026911013e6c1
Owain Kenway <o.kenway@ucl.ac.uk>
2021-10-19 17:04:05 +0100

    Add wildcard to test.

--- test/testexecutor.py
+++ test/testexecutor.py
@@ -1,7 +1,7 @@
 import gepy
 import gepy.executor
 
-js = gepy.executor.qstat_joblist()
+js = gepy.executor.qstat_joblist('*')
 
 for a in js:
     print(a.status, a.jid, a.prio, a.name, a.owner, a.stateblock, a.timeblock, a.slots, a.taskinfo)
