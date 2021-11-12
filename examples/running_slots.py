0fb17ab84a263dfd1ed2eff8e759d0743ca6a650
Owain Kenway <o.kenway@ucl.ac.uk>
2021-11-12 14:36:02 +0000

    Example that counts running slots.

---
+++ examples/running_slots.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import gepy.executor as gepy
+
+jobs = ge.qstat_joblist(filter='*')
+total_running_jobs = 0
+for a in jobs:
+    if 'r' in a.stateblock:
+        total_running_jobs = total_running_jobs + a.slots
+
+print(str(total_running_jobs) + ' slots worth of running jobs.')
