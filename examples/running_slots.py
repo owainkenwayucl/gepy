dac84c5db481f43313c1ba7b8cd9983a2db2246d
Owain Kenway <o.kenway@ucl.ac.uk>
2021-12-03 11:25:23 +0000

    jobs -> tasks

--- examples/running_slots.py
+++ examples/running_slots.py
@@ -11,6 +11,6 @@ for a in jobs:
         total_running_slots = total_running_slots + a.slots
         total_running_tasks = total_running_tasks + 1
 
-print(str(total_running_slots) + ' slots worth of running jobs.')
+print(str(total_running_slots) + ' slots worth of running tasks.')
 print(str(total_running_tasks) + ' running tasks.')
 print(str(total_jobs) + ' jobs in queue.')
