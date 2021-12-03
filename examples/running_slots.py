2300bcb3952703ee630a8a970851b110ef9f7dd3
Owain Kenway <o.kenway@ucl.ac.uk>
2021-12-03 11:23:35 +0000

    Add more detail to running slots example.

--- examples/running_slots.py
+++ examples/running_slots.py
@@ -3,9 +3,14 @@
 import gepy.executor as ge
 
 jobs = ge.qstat_joblist(filter='*')
-total_running_jobs = 0
+total_running_slots = 0
+total_running_tasks = 0
+total_jobs = len(jobs)
 for a in jobs:
     if 'r' in a.stateblock:
-        total_running_jobs = total_running_jobs + a.slots
+        total_running_slots = total_running_slots + a.slots
+        total_running_tasks = total_running_taks + 1
 
-print(str(total_running_jobs) + ' slots worth of running jobs.')
+print(str(total_running_slots) + ' slots worth of running jobs.')
+print(str(total_running_tasks) + ' running tasks.')
+print(str(total_jobs) + ' jobs in queue.')
